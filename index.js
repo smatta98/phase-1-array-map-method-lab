27e6dcd75a3735d2648c4b39d7551200ddba506f
Shiv Matta <shiv.matta@gmail.com>
2022-06-21 23:58:16 -0500

    commit tree a5c3464353aa070a5d58db96098a121ef537e95b

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorials => tutorials.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' '))
 }
+
